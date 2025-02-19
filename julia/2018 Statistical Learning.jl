c95ef4727209a24bc992f30cf3e06f6ff8aba35c
firdaus251 <firdaus251@gmail.com>
2025-02-19 09:52:46 +0300

    commit

--- julia/2018 Statistical Learning.jl
+++ julia/2018 Statistical Learning.jl
@@ -1,4 +1,4 @@
 using PowerModels
 using Ipopt
 
-solve_ac_opf("pglib-opf-17.08/pglib_opf_case3_lmbd.m", Ipopt.Optimizer)
+solve_ac_opf("pglib-opf-17.08/pglib_opf_case13659_pegase.m", Ipopt.Optimizer)
