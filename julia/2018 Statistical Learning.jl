d521a6fdec0a1fccc26ad218ad821e9c2949f235
firdaus251 <firdaus251@gmail.com>
2025-02-19 10:24:01 +0300

    d

--- julia/2018 Statistical Learning.jl
+++ julia/2018 Statistical Learning.jl
@@ -1,5 +1,3 @@
-using JuMP, Clp
-include("NetworkReference.jl")
-data_file = string("/pglib-opf-17.08/pglib_opf_case30_ieee.m")
+using PowerModels, Ipopt
 
-# ref = OPFRecourse.NetworkReference(data_file, bus_prob = 0.95, line_prob = 0.95, σscaling = 0.05);
+solve_ac_opf("pglib-opf-17.08/pglib_opf_case13659_pegase.m", Ipopt.Optimizer)
