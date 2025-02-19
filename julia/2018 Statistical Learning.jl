4502fab63be9d7833e468aade284641c711d475a
firdaus251 <firdaus251@gmail.com>
2025-02-19 16:33:03 +0300

    d

--- julia/2018 Statistical Learning.jl
+++ julia/2018 Statistical Learning.jl
@@ -1,14 +1,1 @@
-# using PowerModels, Ipopt
-# result = solve_dc_opf("pglib-opf-17.08/pglib_opf_case30_ieee.m", Ipopt.Optimizer)
-filename =  "pglib-opf-17.08/pglib_opf_case1951_rte.m"
-network_data = PowerModels.parse_file(filename)
-result = solve_dc_opf(network_data, Ipopt.Optimizer)
-
-result["objective"]
-# ref = PowerModels.build_ref(PowerModels.parse_file(filename))[:nw][0];
-# ref = PowerModels.build_ref(PowerModels.parse_file(filename))
-
-# ref[:it][:pm][:nw][0]
-# result["solve_time"] 2.9539999961853027 dc: 0.0869999
-
-network_data["gen"]["3"]["cost"]
+using PowerModels, Ipopt
